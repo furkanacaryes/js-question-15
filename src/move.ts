b8f20b12195b2fbee49ae6cef009bd647f988c65
Furkan Acar <furkan@acar.digital>
2021-04-04 18:20:37 +0300

    Updated move fn to throw for same folder.

--- src/move.ts
+++ src/move.ts
@@ -1,30 +1,36 @@
-import { List, Folder, File } from './move.typings';
+import { List, Source, Destination } from './move.typings';
+
+// TODO: Add jsdoc
 
 export default function move(list: List, sourceId: string, destId: string): List {
   const listCopy = JSON.parse(JSON.stringify(list)) as List;
 
-  let destFolder: Folder | undefined;
-  let source: { folder: Folder; file: File; index: number } | undefined;
+  let source: Source;
+  let dest: Destination;
 
   listCopy.forEach((folder) => {
     if (folder.id === sourceId) throw new Error('You cannot move a folder');
 
-    if (folder.id === destId) destFolder = folder;
+    if (folder.id === destId) dest = { folder };
 
-    folder.files.forEach((file, index) => {
+    folder.files.forEach((file, fileIndex) => {
       if (file.id === destId) throw new Error('You cannot specify a file as the destination');
 
-      if (file.id === sourceId) source = { folder, file, index };
+      if (file.id === sourceId) {
+        if (folder.id === destId) throw new Error('You cannot move within the same folder');
+
+        source = { folder, file, fileIndex };
+      }
     });
   });
 
   if (!source) throw new Error(`No file could be found with id '${sourceId}'`);
 
-  if (!destFolder) throw new Error(`No folder could be found with id '${destId}'`);
+  if (!dest) throw new Error(`No folder could be found with id '${destId}'`);
 
-  source.folder.files.splice(source.index, 1);
+  source.folder.files.splice(source.fileIndex, 1);
 
-  destFolder.files.push(source.file);
+  dest.folder.files.push(source.file);
 
   return listCopy;
 }
